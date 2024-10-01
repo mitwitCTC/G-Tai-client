1372dbbbd9650942e1a0c162efa56acae3c81e3e
Eileen-Qian <yiqianc.14@gmail.com>
2024-10-01 11:44:28 +0800

    feat: Update FuelRecord layout

--- src/views/FuelRecord.vue
+++ src/views/FuelRecord.vue
@@ -1,6 +1,12 @@
 <script setup></script>
 
 <template>
-  <h1>加油記錄</h1>
-  <router-link to="/">首頁</router-link>
+  <div class="container mt-5">
+    <router-link to="/">
+      <button class="btn btn-outline-warning mb-2">回首頁</button>
+    </router-link>
+    <p class="fw-bold">加油紀錄</p>
+  </div>
 </template>
+
+<style scoped></style>
