da85c922d599bdf8cd8ecac4eca6853fc235faec
Eileen-Qian <yiqianc.14@gmail.com>
2024-10-03 11:20:19 +0800

    feat: Add exportExcel function with file-saver and xlsx

--- src/views/FuelRecord.vue
+++ src/views/FuelRecord.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref, watch, computed, onMounted } from 'vue'
+import FileSaver from 'file-saver'
+import * as XLSX from 'xlsx'
 
 // 預設當月
 const today = new Date()
@@ -239,6 +241,96 @@ function handleSizeChange(size) {
 function handleCurrentChange(page) {
   currentPage.value = page
 }
+
+// 匯出
+function exportExcel() {
+  let xlsxParam = { raw: true }
+  let wb = XLSX.utils.table_to_book(document.querySelector('#fuel_data'), xlsxParam)
+
+  // 獲取工作表
+  let ws = wb.Sheets[wb.SheetNames[0]]
+
+  // 設置儲存格格式
+  let range = XLSX.utils.decode_range(ws['!ref'])
+
+  // 中文欄位名稱與英文變數名稱的對應表
+  const fieldMap = {
+    '數量(公升)': 'quantity',
+    單價: 'unit_price',
+    折讓: 'discount',
+    牌價小計: 'list_price_subtotal',
+    折讓小計: 'discount_subtotal',
+    售價小計: 'subtotal',
+    里程數: 'mileage',
+    油耗: 'fuel_consumption'
+  }
+
+  const numberFields = [
+    'quantity',
+    'unit_price',
+    'discount',
+    'list_price_subtotal',
+    'discount_subtotal',
+    'subtotal',
+    'mileage',
+    'fuel_consumption'
+  ]
+
+  for (let R = range.s.r; R <= range.e.r; ++R) {
+    for (let C = range.s.c; C <= range.e.c; ++C) {
+      let cell_address = { c: C, r: R }
+      let cell_ref = XLSX.utils.encode_cell(cell_address)
+      let cell = ws[cell_ref]
+
+      if (!cell) continue
+      if (!ws[cell_ref].s) ws[cell_ref].s = {}
+
+      // 設置對齊方式
+      ws[cell_ref].s.alignment = { horizontal: 'center', vertical: 'center' }
+
+      // 取得欄位名稱，進行對應映射
+      let columnHeader = ws[XLSX.utils.encode_cell({ c: C, r: 0 })].v.toString().trim()
+
+      // 若欄位名稱在 fieldMap 中，且符合 numberFields
+      if (R > 0 && numberFields.includes(fieldMap[columnHeader])) {
+        cell.t = 'n' // 確保儲存格是數字格式
+        if (isNaN(cell.v)) {
+          cell.v = 0 // 若儲存格的值不是數字，則預設為 0
+        }
+      }
+    }
+  }
+
+  // 設置欄位寬度
+  let colWidths = []
+  for (let C = range.s.c; C <= range.e.c; ++C) {
+    let maxWidth = 14
+    for (let R = range.s.r; R <= range.e.r; ++R) {
+      let cell_address = { c: C, r: R }
+      let cell_ref = XLSX.utils.encode_cell(cell_address)
+      let cell = ws[cell_ref]
+      if (!cell || !cell.v) continue
+      let cellValue = cell.v.toString()
+      maxWidth = Math.max(maxWidth, cellValue.length)
+    }
+    colWidths.push({ wch: maxWidth })
+  }
+  ws['!cols'] = colWidths
+
+  // 將工作簿寫出
+  const wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' })
+  try {
+    FileSaver.saveAs(
+      new Blob([wbout], { type: 'application/octet-stream' }),
+      `${current_month.value}月加油交易明細.xlsx`
+    )
+  } catch (e) {
+    if (console) {
+      console.log(e, wbout)
+    }
+  }
+  return wbout
+}
 </script>
 
 <template>
@@ -280,11 +372,10 @@ function handleCurrentChange(page) {
       format="YYYY-MM"
       value-format="YYYY-MM"
       placeholder="請選擇查詢帳戶月份"
-      @change="fetchFuelData"
     />
     <div class="d-flex justify-content-between align-items-center my-3">
       <p class="m-0">{{ current_month }}月份交易明細</p>
-      <button class="btn btn-warning">匯出</button>
+      <button class="btn btn-warning" @click="exportExcel">匯出</button>
     </div>
     <el-table :data="paginatedData" stripe height="350">
       <el-table-column align="center" min-width="110" prop="team" label="使用單位(對帳單組別)" />
@@ -333,6 +424,34 @@ function handleCurrentChange(page) {
           <span>{{ formatNumber(row.fuel_consumption) }}</span>
         </template>
       </el-table-column>
+    </el-table>
+    <el-table class="d-none" :data="fuel_record" id="fuel_data">
+      <el-table-column align="center" min-width="110" prop="team" label="使用單位(對帳單組別)" />
+      <el-table-column
+        align="center"
+        min-width="110"
+        sortable
+        prop="transaction_date"
+        label="交易日期"
+      />
+      <el-table-column
+        align="center"
+        min-width="110"
+        sortable
+        prop="transaction_time"
+        label="交易時間"
+      />
+      <el-table-column align="center" min-width="110" prop="plate" label="車牌號碼" />
+      <el-table-column align="center" min-width="130" prop="station" label="加油站" />
+      <el-table-column align="center" min-width="110" prop="product_name" label="產品名稱" />
+      <el-table-column align="center" min-width="110" prop="quantity" label="數量(公升)" />
+      <el-table-column align="center" min-width="70" prop="unit_price" label="單價" />
+      <el-table-column align="center" min-width="70" prop="discount" label="折讓" />
+      <el-table-column align="center" min-width="110" prop="list_price_subtotal" label="牌價小計" />
+      <el-table-column align="center" min-width="110" prop="discount_subtotal" label="折讓小計" />
+      <el-table-column align="center" min-width="110" prop="subtotal" label="售價小計" />
+      <el-table-column align="center" min-width="110" prop="mileage" label="里程數" />
+      <el-table-column align="center" min-width="110" prop="fuel_consumption" label="油耗" />
     </el-table>
 
     <p>
