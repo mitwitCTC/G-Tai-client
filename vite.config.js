391605dc59a30e3261a527966f655b22da8e78ac
Eileen-Qian <yiqianc.14@gmail.com>
2024-10-15 14:31:23 +0800

    feat: Remove base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/G-Tai-client',
+  base: '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
