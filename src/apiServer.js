2c233e9d6359d4c8508fee9c641164954fab3fbf
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-12 09:23:28 +0800

    feat: Update server base api path

--- src/apiServer.js
+++ src/apiServer.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://122.116.23.30:3345'
+const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://122.116.23.30:3347'
 
 const apiServer = axios.create({
   baseURL: apiUrl
