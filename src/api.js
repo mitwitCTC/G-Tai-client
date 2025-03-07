8d231d26680ce6375ba21c66027ad0304c275ded
Eileen-Qian <yiqianc.14@gmail.com>
2025-03-07 09:41:18 +0800

    feat: Update base api path

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiUrl = import.meta.env.VITE_API_BASE_URL || 'https://jutai.ct9967.com.tw/'
+const apiUrl = import.meta.env.VITE_API_BASE_URL || 'https://jutai.mitwit-cre.com.tw/'
 
 const apiClient = axios.create({
   baseURL: apiUrl
